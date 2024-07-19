7e506c1909b1e164d16e3718d0d65e64d41a0d4e
Lya389 <131826249+Lya389@users.noreply.github.com>
2024-07-19 19:36:36 +1200

    Attempt 1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    "./website/templates/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
